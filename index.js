bdfcb069d9ca187c225626e8db2ec179817b4496
Aung Thar <samuraizack@gmail.com>
2016-12-17 01:04:49 -0800

    Updated to newest parse server

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+var databaseUri = 'mongodb://SamuraiZack:T1meMuffin1856@ds129095-a0.mlab.com:29095,ds129095-a1.mlab.com:29095/aftercredits?replicaSet=rs-ds129095';
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
@@ -14,7 +14,7 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
+  appId: process.env.APP_ID || 'KIaVlwePeCpkXHNucb21T52C6Nnkmk2ISarDkqEw',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
